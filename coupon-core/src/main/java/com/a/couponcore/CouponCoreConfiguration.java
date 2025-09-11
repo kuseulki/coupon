8c15543baf4d0b8c015537f49482424c159e6ec1
KOO <kuseulki@naver.com>
2025-09-11 17:30:34 +0900

    1. 초기 세팅

---
+++ coupon-core/src/main/java/com/a/couponcore/CouponCoreConfiguration.java
@@ -0,0 +1,12 @@
+package com.a.couponcore;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@ComponentScan
+@EnableAutoConfiguration
+public class CouponCoreConfiguration {
+
+}
